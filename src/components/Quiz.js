07c0df841d3ff7e0832312c2cfa3ab849c0816ab
Ajay Singh <info2azooba@gmail.com>
2022-03-30 16:34:00 +0545

    added codes

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../css/quiz.scss'
 
 function Quiz() {
   return (
-    <div>Quiz</div>
+    <div className='quiz-container'>
+      <h1 className='header'>Header</h1>
+      <h1 className='left-side-bar'>Left Side Bar</h1>
+      <h1 className='main-container'>MCQ Area</h1>
+      <h1 className='right-side-bar'>Right Side Bar</h1>
+      <h1 className='footer'>footer</h1>
+    </div>
   )
 }
 
