67ac4798e22c1da215a627cbabff8ec66072e6b3
Ajay Singh <info2azooba@gmail.com>
2022-03-30 17:02:24 +0545

    scss added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,11 +4,13 @@ import '../css/quiz.scss'
 function Quiz() {
   return (
     <div className='quiz-container'>
-      <h1 className='header'>Header</h1>
-      <h1 className='left-side-bar'>Left Side Bar</h1>
-      <h1 className='main-container'>MCQ Area</h1>
-      <h1 className='right-side-bar'>Right Side Bar</h1>
-      <h1 className='footer'>footer</h1>
+      <div className='quiz-header'>Header</div>
+      <div className='quiz-body'>
+        <div className='left-side-bar'>Left Side Bar</div>
+        <div className='main-container'>MCQ Area</div>
+        <div className='right-side-bar'>Right Side Bar</div>
+      </div>
+      <div className='quiz-footer'>footer</div>
     </div>
   )
 }
